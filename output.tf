a1c598feb9b844f61ecca9cbb62d3c03222de5ff
Craig Thacker <craigthackerx@gmail.com>
2022-04-17 21:13:04 +0100

    fix output

--- output.tf
+++ output.tf
@@ -19,12 +19,12 @@ output "rg_tags" {
 }
 
 output "rg_lock_id" {
-  value       = azurerm_management_lock.resource_group_level_lock.id
+  value       = azurerm_management_lock.rg_lock.id
   description = "The id of the resource group lock"
 }
 
 output "rg_lock_level" {
-  value       = azurerm_management_lock.resource_group_level_lock.lock_level
+  value       = azurerm_management_lock.rg_lock.lock_level
   description = "The lock-level of the resource group lock"
 }
 
